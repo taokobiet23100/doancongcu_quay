4c895a3cb9e27ba6f135dea5b9cac9a1f0d945fa
ngohoanghuy <huyooi1110gmail.i1110gmailcom>
2025-04-09 12:10:51 +0700

    cap nhap code,hoanghuy cap nhap code,ngohoanghu

--- views/category/index.php
+++ views/category/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TRANG SINH VIÊN</title>
+    <title>TRANG BÁN ĐÀN</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         html, body {
